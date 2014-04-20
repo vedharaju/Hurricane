5b091f8ee32ef2157061d58c4b8ba5551c44c028
Zack Drach <hogbait@gmail.com>
2014-04-20 10:18:11 -0400

    created tuple serializer and deserializer

--- src/worker/common.go
+++ src/worker/common.go
@@ -1,2 +1,32 @@
+package main
 
 // Common data structures for the worker nodes
+import "encoding/json"
+import "fmt"
+
+type Tuple struct {
+  slice []string
+}
+
+func MakeTuple(length int) *Tuple {
+  tuple := &Tuple{}
+  tuple.slice = make([]string, length)
+  return tuple
+}
+
+func SerializeTuple(tuple *Tuple) []byte {
+  bytes, err := json.Marshal(tuple.slice)
+  if err != nil {
+    panic(err.Error())
+  }
+  return bytes
+}
+
+func DeserializeTuple(input []byte) *Tuple {
+  tuple := MakeTuple(0)
+  err := json.Unmarshal(input, &tuple.slice)
+  if err != nil {
+    panic(err.Error())
+  }
+  return tuple
+}
