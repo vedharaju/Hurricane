5131f26c8964791a92bff49b65b0546a5fabf280
Zack Drach <hogbait@gmail.com>
2014-04-20 11:21:54 -0400

    read tuples from stream

--- src/worker/common.go
+++ src/worker/common.go
@@ -2,10 +2,13 @@ package worker
 
 // Common data structures for the worker nodes
 import "encoding/json"
+import "io"
 
 type Tuple struct {
   Slice []string
 }
+
+type TupleFunc func(Tuple)
 
 func MakeTuple(length int) Tuple {
   tuple := Tuple{}
@@ -28,4 +31,25 @@ func DeserializeTuple(input []byte) Tuple {
     panic(err.Error())
   }
   return Tuple{slice}
+}
+
+func ReadTupleStream(reader io.Reader, callback TupleFunc) {
+  buff := make([]byte, 4096)
+  line := make([]byte, 0, 4096)
+  for {
+    n, err := reader.Read(buff)
+    if err != nil {
+      return
+    }
+    start := 0
+    for i:=0; i<n; i++ {
+      if buff[i] == '\n' {
+        line = append(line, buff[start:i]...)
+        start = i+1
+        callback(DeserializeTuple(line))
+        line = line[0:0]
+      }
+    }
+    line = append(line, buff[start:n]...)
+  }
 }
--- src/worker/common_test.go
+++ src/worker/common_test.go
@@ -2,6 +2,8 @@ package worker;
 
 import "testing"
 import "reflect"
+import "bytes"
+import "strconv"
 
 func TestSerializeDeserialize(t *testing.T) {
   tuple1 := MakeTuple(2)
@@ -12,6 +14,28 @@ func TestSerializeDeserialize(t *testing.T) {
 
   if !reflect.DeepEqual(tuple1, tuple2) {
     t.Errorf("Failure %s != %s", tuple1, tuple2)
+  }
+}
+
+func TestReadTupleStream(t *testing.T) {
+  data := make([]byte,0)
+  oldsum := 0
+  for i:=0; i<5; i++ {
+    tuple := MakeTuple(1)
+    tuple.Slice[0] = strconv.Itoa(i)
+    oldsum += i
+    data = append(data, tuple.SerializeTuple()...)
+    data = append(data, '\n')
+  }
+
+  newsum := 0
+  ReadTupleStream(bytes.NewBuffer(data), func (t Tuple) {
+    num, _ := strconv.Atoi(t.Slice[0])
+    newsum += num
+  })
+
+  if oldsum != newsum {
+    t.Errorf("Failure %d != %d", oldsum, newsum)
   }
 }
 
