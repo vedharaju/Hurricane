ea2ca4fd517fb6adb4b81d6fd2f5df7a90029d71
Jeffrey Warren <jtwarren@mit.edu>
2014-04-21 17:06:17 -0400

    Basic querying and querying testing done

--- src/master/queries.go
+++ src/master/queries.go
@@ -4,22 +4,12 @@ import (
 	hood "github.com/eaigner/hood"
 )
 
-func (rdd *Rdd) getSegments(tx *hood.Hood) []*Segment {
-	var results []*Segment
-	err := tx.Where("RddId", "=", rdd.Id).Find(&results)
+func (rdd *Rdd) getSegments(tx *hood.Hood) []Segment {
+	var results []Segment
+	err := tx.Where("rdd_id", "=", rdd.Id).Find(&results)
 	if err != nil {
 		panic(err)
 	}
 
 	return results
-}
-
-func (rdd *Rdd) getJob(tx *hood.Hood) *Job {
-  var results []*Job
-  err := tx.Where("JobId", "=", rdd.JobId).Limit(1).Find(&results)
-  if err != nil {
-    panic(err)
-  }
-
-  return results[0]
 }
---
+++ src/master/queries_test.go
@@ -0,0 +1,41 @@
+package master
+
+import (
+	"fmt"
+	"github.com/eaigner/hood"
+	"testing"
+)
+
+func setup(hd *hood.Hood) {
+	ResetDb(hd)
+	CreateTables(hd)
+}
+
+func cleanup(hd *hood.Hood) {
+	ResetDb(hd)
+}
+
+func TestBasic(t *testing.T) {
+	hd := GetTestDbConnection()
+  setup(hd)
+  defer cleanup(hd)
+
+	fmt.Printf("Test: Basic RDD has segments ...\n")
+
+	workflow := MockWorkflow(hd)
+	protojob := MockProtojob(hd, workflow)
+	workflowBatch := MockWorkflowBatch(hd, workflow)
+	rdd := MockRdd(hd, workflowBatch, protojob)
+	worker := MockWorker(hd)
+	MockSegment(hd, rdd, worker)
+  MockSegment(hd, rdd, worker)
+
+  results := rdd.getSegments(hd)
+
+  if len(results) != 2 {
+    t.Fatalf("incorrect number of segments; wanted=%d got=%d", len(results), 2)
+  }
+
+	fmt.Printf("  ... Passed\n")
+
+}
