036abe05c10d9fc168a01453b505ea9a1689a047
Zack Drach <hogbait@zdrach.com>
2014-04-21 22:41:59 +0000

    added indexes to models

--- src/master/models.go
+++ src/master/models.go
@@ -9,13 +9,18 @@ type RddEdge struct {
 	// Auto-incrementing int field 'id'
 	Id hood.Id
 
-  // References to the Rdd table
-  SourceRddId int64
-  DestRddId int64
+	// References to the Rdd table
+	SourceRddId int64
+	DestRddId   int64
 
 	// These fields are auto updated on save
 	Created hood.Created
 	Updated hood.Updated
+}
+
+func (table *RddEdge) Indexes(indexes *hood.Indexes) {
+	indexes.Add("rdd_edge__source_rdd_id", "source_rdd_id")
+	indexes.Add("rdd_edge__dest_rdd_id", "dest_rdd_id")
 }
 
 // RDD and "Job" are stored in the same data structure because every
@@ -35,6 +40,11 @@ type Rdd struct {
 	Updated hood.Updated
 }
 
+func (table *Rdd) Indexes(indexes *hood.Indexes) {
+	indexes.Add("rdd__workflow_batch_id", "workflow_batch_id")
+	indexes.Add("rdd__protojob_id", "protojob_id")
+}
+
 // Segments and "Tasks" are store in the same data structure
 // because each segment is produced by exactly one task
 type Segment struct {
@@ -52,6 +62,11 @@ type Segment struct {
 	Updated hood.Updated
 }
 
+func (table *Segment) Indexes(indexes *hood.Indexes) {
+	indexes.Add("segment__rdd_id", "rdd_id")
+	indexes.Add("segment__worker_id", "worker_id")
+}
+
 type Workflow struct {
 	// Auto-incrementing int field 'id'
 	Id hood.Id
@@ -65,21 +80,26 @@ type WorkflowEdge struct {
 	// Auto-incrementing int field 'id'
 	Id hood.Id
 
-  // References into Protojob table
-  SourceJobId int64
-  DestJobId int64
+	// References into Protojob table
+	SourceJobId int64
+	DestJobId   int64
 
 	// These fields are auto updated on save
 	Created hood.Created
 	Updated hood.Updated
 }
 
+func (table *WorkflowEdge) Indexes(indexes *hood.Indexes) {
+	indexes.Add("workflow_edge__source_job_id", "source_job_id")
+	indexes.Add("workflow_edge__dest_job_id", "dest_job_id")
+}
+
 type Protojob struct {
 	// Auto-incrementing int field 'id'
 	Id hood.Id
 
-  // Reference to the workflow
-  WorkflowId int64
+	// Reference to the workflow
+	WorkflowId int64
 
 	// Name of the command, UDF or built-in
 	Command string
@@ -87,6 +107,10 @@ type Protojob struct {
 	// These fields are auto updated on save
 	Created hood.Created
 	Updated hood.Updated
+}
+
+func (table *Protojob) Indexes(indexes *hood.Indexes) {
+	indexes.Add("protojob__workflow_id", "workflow_id")
 }
 
 type WorkflowBatch struct {
@@ -105,6 +129,10 @@ type WorkflowBatch struct {
 	// These fields are auto updated on save
 	Created hood.Created
 	Updated hood.Updated
+}
+
+func (table *WorkflowBatch) Indexes(indexes *hood.Indexes) {
+	indexes.Add("workflow_batch__workflow_id", "workflow_id")
 }
 
 type Worker struct {
