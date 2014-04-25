370c68c1fdcbfff664fc664c42d7c6b0f89e6dca
Zack Drach <hogbait@gmail.com>
2014-04-25 16:51:40 +0000

    ran gofmt on worker directory

--- src/worker/common.go
+++ src/worker/common.go
@@ -5,73 +5,73 @@ import "encoding/json"
 import "io"
 
 type Tuple struct {
-  Slice []string
+	Slice []string
 }
 
 type TupleFunc func(Tuple, int)
 
 func MakeTuple(length int) Tuple {
-  tuple := Tuple{}
-  tuple.Slice = make([]string, length)
-  return tuple
+	tuple := Tuple{}
+	tuple.Slice = make([]string, length)
+	return tuple
 }
 
 // Serialize a tuple into a json blob of the following form:
 //   [["word1", "word2", "word3"], 4]
 // where the words are from tuple.Slice, and the number is the index
 func (tuple Tuple) SerializeTuple(index int) []byte {
-  bytes, err := json.Marshal([]interface{}{tuple.Slice, index})
-  if err != nil {
-    panic(err.Error())
-  }
-  return bytes
+	bytes, err := json.Marshal([]interface{}{tuple.Slice, index})
+	if err != nil {
+		panic(err.Error())
+	}
+	return bytes
 }
 
 // Opposite of SerializeTuple
 func DeserializeTuple(input []byte) (Tuple, int) {
-  var output []interface{}
-  err := json.Unmarshal(input, &output)
-  if err != nil {
-    panic(err.Error())
-  }
-  outputList := output[0].([]interface{})
-  slice := make([]string, len(outputList))
-  for i := range slice {
-    slice[i] = outputList[i].(string)
-  }
-  return Tuple{slice}, int(output[1].(float64))
+	var output []interface{}
+	err := json.Unmarshal(input, &output)
+	if err != nil {
+		panic(err.Error())
+	}
+	outputList := output[0].([]interface{})
+	slice := make([]string, len(outputList))
+	for i := range slice {
+		slice[i] = outputList[i].(string)
+	}
+	return Tuple{slice}, int(output[1].(float64))
 }
 
 // Read a stream of serialized tuples, deserialize those tuples,
 // and execute the callback function for each one of those tuples.
 func ReadTupleStream(reader io.Reader, callback TupleFunc) {
-  buff := make([]byte, 4096)
-  line := make([]byte, 0, 4096)
-  for {
-    n, err := reader.Read(buff)
-    if err != nil {
-      return
-    }
-    start := 0
-    for i:=0; i<n; i++ {
-      if buff[i] == '\n' {
-        line = append(line, buff[start:i]...)
-        start = i+1
-        tuple, index := DeserializeTuple(line)
-        callback(tuple, index)
-        line = line[0:0]
-      }
-    }
-    line = append(line, buff[start:n]...)
-  }
+	buff := make([]byte, 4096)
+	line := make([]byte, 0, 4096)
+	for {
+		n, err := reader.Read(buff)
+		if err != nil {
+			return
+		}
+		start := 0
+		for i := 0; i < n; i++ {
+			if buff[i] == '\n' {
+				line = append(line, buff[start:i]...)
+				start = i + 1
+				tuple, index := DeserializeTuple(line)
+				callback(tuple, index)
+				line = line[0:0]
+			}
+		}
+		line = append(line, buff[start:n]...)
+	}
 }
 
 type Segment struct {
-  Tuples []Tuple
+	Tuples []Tuple
 }
 
 func MakeSegment(tuples []Tuple) Segment {
-  var segment Segment
-  segment.Tuples = tuples
-  return segment
+	var segment Segment
+	segment.Tuples = tuples
+	return segment
 }
--- src/worker/common_test.go
+++ src/worker/common_test.go
@@ -1,4 +1,4 @@
-package worker;
+package worker
 
 import "testing"
 import "reflect"
@@ -7,70 +7,70 @@ import "fmt"
 import "strconv"
 
 func TestSerializeDeserialize(t *testing.T) {
-  tuple1 := MakeTuple(2)
-  tuple1.Slice[0] = "MOO"
-  tuple1.Slice[1] = "abc"
+	tuple1 := MakeTuple(2)
+	tuple1.Slice[0] = "MOO"
+	tuple1.Slice[1] = "abc"
 
-  tuple2, index := DeserializeTuple(tuple1.SerializeTuple(1))
+	tuple2, index := DeserializeTuple(tuple1.SerializeTuple(1))
 
-  if !reflect.DeepEqual(tuple1, tuple2) {
-    t.Errorf("Failure %s != %s", tuple1, tuple2)
-  }
+	if !reflect.DeepEqual(tuple1, tuple2) {
+		t.Errorf("Failure %s != %s", tuple1, tuple2)
+	}
 
-  if index != 1 {
-    t.Errorf("Failure, index %d != 1", index)
-  }
+	if index != 1 {
+		t.Errorf("Failure, index %d != 1", index)
+	}
 }
 
 func TestReadTupleStream(t *testing.T) {
-  data := make([]byte,0)
-  oldsum := 0
-  for i:=0; i<5; i++ {
-    tuple := MakeTuple(1)
-    tuple.Slice[0] = strconv.Itoa(i)
-    oldsum += i
-    data = append(data, tuple.SerializeTuple(4)...)
-    data = append(data, '\n')
-  }
+	data := make([]byte, 0)
+	oldsum := 0
+	for i := 0; i < 5; i++ {
+		tuple := MakeTuple(1)
+		tuple.Slice[0] = strconv.Itoa(i)
+		oldsum += i
+		data = append(data, tuple.SerializeTuple(4)...)
+		data = append(data, '\n')
+	}
 
-  newsum := 0
-  ReadTupleStream(bytes.NewBuffer(data), func (tuple Tuple, index int) {
-    num, _ := strconv.Atoi(tuple.Slice[0])
-    newsum += num
+	newsum := 0
+	ReadTupleStream(bytes.NewBuffer(data), func(tuple Tuple, index int) {
+		num, _ := strconv.Atoi(tuple.Slice[0])
+		newsum += num
 
-    if index != 4 {
-      t.Errorf("Failure index %d != 4", index)
-    }
-  })
+		if index != 4 {
+			t.Errorf("Failure index %d != 4", index)
+		}
+	})
 
-  if oldsum != newsum {
-    t.Errorf("Failure %d != %d", oldsum, newsum)
-  }
+	if oldsum != newsum {
+		t.Errorf("Failure %d != %d", oldsum, newsum)
+	}
 }
 
 func TestBasicSegment(t *testing.T) {
-  fmt.Printf("Test: Basic Segment has tuples ...\n")
+	fmt.Printf("Test: Basic Segment has tuples ...\n")
 
-  tuple1 := MakeTuple(2)
-  tuple1.Slice[0] = "MOO"
-  tuple1.Slice[1] = "abc"
+	tuple1 := MakeTuple(2)
+	tuple1.Slice[0] = "MOO"
+	tuple1.Slice[1] = "abc"
 
-  tuple2 := MakeTuple(2)
-  tuple2.Slice[0] = "OINK"
-  tuple2.Slice[1] = "cde"
+	tuple2 := MakeTuple(2)
+	tuple2.Slice[0] = "OINK"
+	tuple2.Slice[1] = "cde"
 
-  tuples := []Tuple{tuple1, tuple2}
+	tuples := []Tuple{tuple1, tuple2}
 
-  segment := MakeSegment(tuples)
+	segment := MakeSegment(tuples)
 
-  for i, tuple := range segment.Tuples {
-    if tuple.Slice[0] != tuples[i].Slice[0] {
-      t.Errorf("Failure %s != %s", tuple.Slice[0], tuples[i].Slice[0])
-    }
-    if tuple.Slice[1] != tuples[i].Slice[1] {
-      t.Errorf("Failure %s != %s", tuple.Slice[1], tuples[i].Slice[1])
-    }
-  }
+	for i, tuple := range segment.Tuples {
+		if tuple.Slice[0] != tuples[i].Slice[0] {
+			t.Errorf("Failure %s != %s", tuple.Slice[0], tuples[i].Slice[0])
+		}
+		if tuple.Slice[1] != tuples[i].Slice[1] {
+			t.Errorf("Failure %s != %s", tuple.Slice[1], tuples[i].Slice[1])
+		}
+	}
 
-  fmt.Printf("  ... Passed\n")
+	fmt.Printf("  ... Passed\n")
 }
