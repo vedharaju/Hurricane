2afd61715ac3c2a512aa0f07bbaa8042c664b905
Zack Drach <hogbait@gmail.com>
2014-04-25 19:12:56 +0000

    added and tested more database queries

--- src/master/queries.go
+++ src/master/queries.go
@@ -1,7 +1,7 @@
 package master
 
 import (
-	hood "github.com/eaigner/hood"
+	"github.com/eaigner/hood"
 )
 
 func (rdd *Rdd) getSegments(tx *hood.Hood) []*Segment {
@@ -14,6 +14,109 @@ func (rdd *Rdd) getSegments(tx *hood.Hood) []*Segment {
 	// Should return pointers to the result objects so that
 	// they can be mutated
 	pointerResults := make([]*Segment, len(results))
+	for i := range results {
+		pointerResults[i] = &results[i]
+	}
+
+	return pointerResults
+}
+
+func (workflow *Workflow) getProtojobs(tx *hood.Hood) []*Protojob {
+	var results []Protojob
+	err := tx.Where("workflow_id", "=", workflow.Id).Find(&results)
+	if err != nil {
+		panic(err)
+	}
+
+	// Should return pointers to the result objects so that
+	// they can be mutated
+	pointerResults := make([]*Protojob, len(results))
+	for i := range results {
+		pointerResults[i] = &results[i]
+	}
+
+	return pointerResults
+}
+
+// Select all workflow edges whose dest_protojob is in the given
+// workflow (this also imlies that the source_protojob is in the workflow)
+func (workflow *Workflow) getWorkflowEdges(tx *hood.Hood) []*WorkflowEdge {
+	var results []WorkflowEdge
+	err := tx.FindSql(&results,
+		`select *
+    from workflow_edge
+    left join protojob dest_job
+    on workflow_edge.dest_job_id = dest_job.id
+    where dest_job.workflow_id = $1`, workflow.Id)
+	if err != nil {
+		panic(err)
+	}
+
+	// Should return pointers to the result objects so that
+	// they can be mutated
+	pointerResults := make([]*WorkflowEdge, len(results))
+	for i := range results {
+		pointerResults[i] = &results[i]
+	}
+
+	return pointerResults
+}
+
+// Get all Rdd edges whose source or dest Rdd is in the given workflow
+// batch. Note that the source and dest Rdds may be produced in different
+// batches.
+func (workflowBatch *WorkflowBatch) getRddEdges(tx *hood.Hood) []*RddEdge {
+	var results []RddEdge
+	err := tx.FindSql(&results,
+		`select *
+    from rdd_edge
+    left join rdd source_rdd
+    on rdd_edge.source_rdd_id = source_rdd.id
+    left join rdd dest_rdd
+    on rdd_edge.dest_rdd_id = dest_rdd.id
+    where source_rdd.workflow_batch_id = $1
+    or dest_rdd.workflow_batch_id = $1`, workflowBatch.Id)
+	if err != nil {
+		panic(err)
+	}
+
+	// Should return pointers to the result objects so that
+	// they can be mutated
+	pointerResults := make([]*RddEdge, len(results))
+	for i := range results {
+		pointerResults[i] = &results[i]
+	}
+
+	return pointerResults
+}
+
+func (workflow *Workflow) getWorkflowBatches(tx *hood.Hood) []*WorkflowBatch {
+	var results []WorkflowBatch
+	err := tx.Where("workflow_id", "=", workflow.Id).Find(&results)
+	if err != nil {
+		panic(err)
+	}
+
+	// Should return pointers to the result objects so that
+	// they can be mutated
+	pointerResults := make([]*WorkflowBatch, len(results))
+	for i := range results {
+		pointerResults[i] = &results[i]
+	}
+
+	return pointerResults
+}
+
+func (workflowBatch *WorkflowBatch) getRdds(tx *hood.Hood) []*Rdd {
+	var results []Rdd
+	err := tx.Where("workflow_batch_id", "=", workflowBatch.Id).Find(&results)
+	if err != nil {
+		panic(err)
+	}
+
+	// Should return pointers to the result objects so that
+	// they can be mutated
+	pointerResults := make([]*Rdd, len(results))
 	for i := range results {
 		pointerResults[i] = &results[i]
 	}
--- src/master/queries_test.go
+++ src/master/queries_test.go
@@ -11,10 +11,6 @@ func setup(hd *hood.Hood) {
 	CreateTables(hd)
 }
 
-func cleanup_database(hd *hood.Hood) {
-	ResetDb(hd)
-}
-
 func TestBasic(t *testing.T) {
 	hd := GetTestDbConnection()
 	setup(hd)
@@ -33,7 +29,7 @@ func TestBasic(t *testing.T) {
 	results := rdd.getSegments(hd)
 
 	if len(results) != 2 {
-		t.Fatalf("incorrect number of segments; wanted=%d got=%d", len(results), 2)
+		t.Fatalf("incorrect number of segments; got=%d wanted=%d", len(results), 2)
 	}
 
 	fmt.Printf("  ... Passed\n")
@@ -43,10 +39,37 @@ func TestBasic(t *testing.T) {
 func TestComplex(t *testing.T) {
 	hd := GetTestDbConnection()
 	setup(hd)
-	defer cleanup_database(hd)
 
 	fmt.Printf("Test: Diamond Workflow Queries ...\n")
 
-	// TODO: Finish this.
+	workflow := MockDiamondWorkflow(hd)
 
+	// test protojobs
+	pjs := workflow.getProtojobs(hd)
+	if len(pjs) != 4 {
+		t.Fatalf("incorrect number of protojobs; got=%d wanted=%d", len(pjs), 4)
+	}
+
+	// test edges
+	wes := workflow.getWorkflowEdges(hd)
+	if len(wes) != 4 {
+		t.Fatalf("incorrect number of workflow edges; got=%d wanted=%d", len(wes), 4)
+	}
+
+	// test batches
+	wbs := workflow.getWorkflowBatches(hd)
+	if len(wbs) != 1 {
+		t.Fatalf("incorrect number of workflow batches; got=%d wanted=%d", len(wbs), 1)
+	}
+
+	wb := wbs[0]
+	rdds := wb.getRdds(hd)
+	if len(rdds) != 4 {
+		t.Fatalf("incorrect number of rdds; got=%d wanted=%d", len(wbs), 4)
+	}
+
+	rddes := wb.getRddEdges(hd)
+	if len(rddes) != 4 {
+		t.Fatalf("incorrect number of rdd edges; got=%d wanted=%d", len(wbs), 4)
+	}
 }
