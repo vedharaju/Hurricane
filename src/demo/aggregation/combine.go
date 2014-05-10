094ba310d29bfe68bad48a628b4eb832ed5d9f12
vikasvelagapudi <vvelagapudi0717@gmail.com>
2014-05-10 15:24:41 -0400

    Add working running sum, over past 3 inputs

--- src/demo/aggregation/combine.go
+++ src/demo/aggregation/combine.go
@@ -4,35 +4,22 @@ import "os"
 import "syscall"
 import "worker"
 import "strconv"
-import "time"
 
 func main() {
 
-  file, err := os.Open("input-combine-" + time.Now().String())
-  if err != nil {
-    file, err = os.Create("input-combine-" + time.Now().String())
-    if err != nil {
-      panic(err)
-    }
-  }
-  defer file.Close()
-
-
   // read from standard input to get the input tuples
   newTuples := make([]worker.Tuple, 0)
-//  oldTuples := make([]worker.Tuple, 0)*/
+  oldTuples := make([]worker.Tuple, 0)
   totalTuples := make([]worker.Tuple, 0)
 
   worker.ReadTupleStream(os.Stdin, func(tuple worker.Tuple, index int) {
-    file.Write([]byte(tuple.SerializeTuple(index)))
-
     switch(index) {
-      case 1:
-        newTuples = append(newTuples, tuple)
-//    case 1:
-  //      oldTuples = append(oldTuples, tuple)*/
-      case 0:
-        totalTuples = append(totalTuples, tuple)
+    case 0:
+      newTuples = append(newTuples, tuple)
+    case 1:
+      oldTuples = append(oldTuples, tuple)
+    case 2:
+      totalTuples = append(totalTuples, tuple)
     }
   })
 
@@ -60,27 +47,16 @@ func main() {
     }
   }
 
-//  for _, tuple := range oldTuples {
-//    word := tuple.Slice[0]
-//
-  //  if count, ok := counts[word]; ok {
-    //  counts[word] = count - 1
-//    }
-  //}
+  for _, tuple := range oldTuples {
+    word := tuple.Slice[0]
 
-  file1, err := os.Open("prod-combine-" + time.Now().String())
-  if err != nil {
-    file1, err = os.Create("prod-combine-" + time.Now().String())
-    if err != nil {
-      panic(err)
+    if count, ok := counts[word]; ok {
+      counts[word] = count - 1
     }
   }
-  defer file1.Close()
-
 
   for word, count := range counts {
     outTuple := worker.Tuple{[]string{word, strconv.Itoa(count)}}
-    file1.Write([]byte(outTuple.SerializeTuple(2)))
 
     stdout.Write(outTuple.SerializeTuple(2))
     stdout.Write([]byte{'\n'})
