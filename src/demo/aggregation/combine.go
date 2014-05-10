9265e8bb9bb12b4e908b47923fba174aa5814c82
vikasvelagapudi <vvelagapudi0717@gmail.com>
2014-05-10 16:16:19 -0400

    Update aggregate workflow to append to single file

--- src/demo/aggregation/combine.go
+++ src/demo/aggregation/combine.go
@@ -7,58 +7,58 @@ import "strconv"
 
 func main() {
 
-  // read from standard input to get the input tuples
-  newTuples := make([]worker.Tuple, 0)
-  oldTuples := make([]worker.Tuple, 0)
-  totalTuples := make([]worker.Tuple, 0)
+	// read from standard input to get the input tuples
+	newTuples := make([]worker.Tuple, 0)
+	oldTuples := make([]worker.Tuple, 0)
+	totalTuples := make([]worker.Tuple, 0)
 
-  worker.ReadTupleStream(os.Stdin, func(tuple worker.Tuple, index int) {
-    switch(index) {
-    case 0:
-      newTuples = append(newTuples, tuple)
-    case 1:
-      oldTuples = append(oldTuples, tuple)
-    case 2:
-      totalTuples = append(totalTuples, tuple)
-    }
-  })
+	worker.ReadTupleStream(os.Stdin, func(tuple worker.Tuple, index int) {
+		switch index {
+		case 0:
+			newTuples = append(newTuples, tuple)
+		case 1:
+			oldTuples = append(oldTuples, tuple)
+		case 2:
+			totalTuples = append(totalTuples, tuple)
+		}
+	})
 
-  stdout := os.NewFile(uintptr(syscall.Stdout), "/dev/stdout")
+	stdout := os.NewFile(uintptr(syscall.Stdout), "/dev/stdout")
 
-  counts := make(map[string]int)
-  for _, tuple := range totalTuples {
-    word := tuple.Slice[0]
-    prev, _ := strconv.Atoi(tuple.Slice[1])
+	counts := make(map[string]int)
+	for _, tuple := range totalTuples {
+		word := tuple.Slice[0]
+		prev, _ := strconv.Atoi(tuple.Slice[1])
 
-    if count, ok := counts[word]; ok {
-      counts[word] = count + prev
-    } else {
-      counts[word] = prev
-    }
-  }
+		if count, ok := counts[word]; ok {
+			counts[word] = count + prev
+		} else {
+			counts[word] = prev
+		}
+	}
 
-  for _, tuple := range newTuples {
-    word := tuple.Slice[0]
+	for _, tuple := range newTuples {
+		word := tuple.Slice[0]
 
-    if count, ok := counts[word]; ok {
-      counts[word] = count + 1
-    } else {
-      counts[word] = 1
-    }
-  }
+		if count, ok := counts[word]; ok {
+			counts[word] = count + 1
+		} else {
+			counts[word] = 1
+		}
+	}
 
-  for _, tuple := range oldTuples {
-    word := tuple.Slice[0]
+	for _, tuple := range oldTuples {
+		word := tuple.Slice[0]
 
-    if count, ok := counts[word]; ok {
-      counts[word] = count - 1
-    }
-  }
+		if count, ok := counts[word]; ok {
+			counts[word] = count - 1
+		}
+	}
 
-  for word, count := range counts {
-    outTuple := worker.Tuple{[]string{word, strconv.Itoa(count)}}
+	for word, count := range counts {
+		outTuple := worker.Tuple{[]string{word, strconv.Itoa(count)}}
 
-    stdout.Write(outTuple.SerializeTuple(2))
-    stdout.Write([]byte{'\n'})
-  }
+		stdout.Write(outTuple.SerializeTuple(2))
+		stdout.Write([]byte{'\n'})
+	}
 }
--- src/demo/aggregation/output.go
+++ src/demo/aggregation/output.go
@@ -6,17 +6,14 @@ import "time"
 
 func main() {
 
-  file, err := os.Open("output.txt" + time.Now().String())
-  if err != nil {
-    file, err = os.Create("output.txt" + time.Now().String())
-    if err != nil {
-      panic(err)
-    }
-  }
-  defer file.Close()
+	file, err := os.OpenFile("output.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	file.Write([]byte(time.Now().String() + "\n"))
+	defer file.Close()
 
-  worker.ReadTupleStream(os.Stdin, func(tuple worker.Tuple, index int) {
-    file.Write([]byte(tuple.SerializeTuple(index)))
-    file.Write([]byte(tuple.Slice[0] + ", " + tuple.Slice[1] + "\n"))
-  })
+	worker.ReadTupleStream(os.Stdin, func(tuple worker.Tuple, index int) {
+		file.Write([]byte(tuple.Slice[0] + ", " + tuple.Slice[1] + "\n"))
+	})
 }
